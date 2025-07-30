85bff0f9c7ccaeb33147f092b5b86095bc93db4a
Lera <admin@MacBook-Pro--Lera.local>
2025-07-30 17:18:55 +0300

    add test search

---
+++ e2e/tests/search.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+test('should navigate to first article when pressing \'enter\'', async ({ page }) => {
+    await page.goto('/en-US/');
+
+    const searchInput = page.locator('#top-nav-search-input');
+    await expect(searchInput).toBeEditable();
+    await searchInput.fill('Web');
+
+    const firstElement = page.locator('div.search-results div:first-child a');
+    await expect(firstElement).toBeVisible();
+
+    const articleName = await firstElement.locator('b').innerText();
+    const articleTitle = articleName + ' | MDN';
+
+    await Promise.all([
+        page.waitForLoadState('load'),
+        searchInput.press('Enter')
+    ])
+
+    await expect(page).toHaveTitle(articleTitle);
+})
